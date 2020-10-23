1e56454eb94f4566e75731800bb2cf06eeaedaa0
Osh Slomczynski <8842282+oshdev@users.noreply.github.com>
2020-10-23 11:23:08 +0100

    Update Uncontrolled.js

--- src/Uncontrolled.js
+++ src/Uncontrolled.js
@@ -6,7 +6,7 @@ const Uncontrolled = () => {
     if (!userRef.current.value) { alert('User must be provided'); event.preventDefault() }
   } // value comes from the DOM
   return (
-    <form method="POST" target="/login" onSubmit={validateInput}>
+    <form method="POST" action="/login" onSubmit={validateInput}>
       <label>
         User <input type="text" name="user" ref={userRef}/>{/* reference the element */}
       </label>
