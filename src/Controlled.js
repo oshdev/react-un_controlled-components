a0771d3f0fa2c8012733f786cd48130928623445
Osh Slomczynski <8842282+oshdev@users.noreply.github.com>
2020-10-23 11:22:48 +0100

    Update Controlled.js

--- src/Controlled.js
+++ src/Controlled.js
@@ -6,7 +6,7 @@ const Controlled = () => {
     if (!user) { alert('User must be provided'); event.preventDefault() }
   } // value comes from the state hook
   return (
-    <form method="POST" target="/login" onSubmit={validateInput}>
+    <form method="POST" action="/login" onSubmit={validateInput}>
       <label>
         User <input type="text" name="user" value={user} // populating the value
             onChange={(event) => setUser(event.target.value)} />{/* updating state */}
